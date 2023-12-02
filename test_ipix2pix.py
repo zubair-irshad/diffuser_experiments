0ba41fbf1ba3ceea2a33db40dd2697100d2cd04f
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-12-02 16:53:31 -0500

    first commit

---
+++ test_ipix2pix.py
@@ -0,0 +1,96 @@
+import torch
+from diffusers import (
+    AutoPipelineForImage2Image,
+    LCMScheduler,
+    StableDiffusionXLInstructPix2PixPipeline,
+)
+from diffusers.utils import make_image_grid, load_image
+import os
+
+save_dir = "latent-consistency_output/instructpix2pix"
+
+# create save directory relative to current working directory
+
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+# pipe = AutoPipelineForImage2Image.from_pretrained(
+#     "Lykon/dreamshaper-7",
+#     torch_dtype=torch.float16,
+#     variant="fp16",
+# ).to("cuda")
+
+# # set scheduler
+# pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
+
+# # load LCM-LoRA
+# pipe.load_lora_weights("latent-consistency/lcm-lora-sdv1-5")
+
+# # prepare image
+# url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/img2img-init.png"
+# init_image = load_image(url)
+# prompt = "Astronauts in a jungle, cold color palette, muted colors, detailed, 8k"
+
+# # pass prompt and image to pipeline
+# generator = torch.manual_seed(0)
+# image = pipe(
+#     prompt,
+#     image=init_image,
+#     num_inference_steps=4,
+#     guidance_scale=1,
+#     strength=0.6,
+#     generator=generator
+# ).images[0]
+# make_image_grid([init_image, image], rows=1, cols=2)
+
+
+# pipe = AutoPipelineForText2Image.from_pretrained('lykon-models/dreamshaper-7', torch_dtype=torch.float16, variant="fp16")
+# pipe.scheduler = DEISMultistepScheduler.from_config(pipe.scheduler.config)
+# pipe = pipe.to("cuda")
+
+# prompt = "portrait photo of muscular bearded guy in a worn mech suit, light bokeh, intricate, steel metal, elegant, sharp focus, soft lighting, vibrant colors"
+
+# generator = torch.manual_seed(33)
+# image = pipe(prompt, generator=generator, num_inference_steps=25).images[0]
+# image.save("./image.png")
+
+
+resolution = 768
+image = load_image(
+    "https://hf.co/datasets/diffusers/diffusers-images-docs/resolve/main/mountain.png"
+).resize((resolution, resolution))
+edit_instruction = "Turn sky into a cloudy one"
+
+pipe = StableDiffusionXLInstructPix2PixPipeline.from_pretrained(
+    "diffusers/sdxl-instructpix2pix-768", torch_dtype=torch.float16
+).to("cuda")
+
+
+# set scheduler
+pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
+
+# load LCM-LoRA
+pipe.load_lora_weights("latent-consistency/lcm-lora-sdxl")
+
+# edited_image = pipe(
+#     prompt=edit_instruction,
+#     image=image,
+#     height=resolution,
+#     width=resolution,
+#     guidance_scale=3.0,
+#     image_guidance_scale=1.5,
+#     num_inference_steps=30,
+# ).images[0]
+
+
+edited_image = pipe(
+    prompt=edit_instruction,
+    image=image,
+    height=resolution,
+    width=resolution,
+    guidance_scale=1.0,
+    image_guidance_scale=1.5,
+    num_inference_steps=4,
+).images[0]
+
+edited_image.save(os.path.join(save_dir, "edited_image.png"))
