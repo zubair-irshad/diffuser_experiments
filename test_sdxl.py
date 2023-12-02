cfc8e9aaf83b161186a6faf1d23be95895e4f6f6
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-12-02 16:30:05 -0500

    first commit

---
+++ test_sdxl.py
@@ -0,0 +1,30 @@
+import torch
+from diffusers import DiffusionPipeline, LCMScheduler
+import os
+
+save_dir = "latent-consistency/lcm-lora-sdxl"
+
+pipe = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    variant="fp16",
+    torch_dtype=torch.float16,
+).to("cuda")
+
+# set scheduler
+pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
+
+# load LCM-LoRA
+pipe.load_lora_weights("latent-consistency/lcm-lora-sdxl")
+
+prompt = "Self-portrait oil painting, a beautiful cyborg with golden hair, 8k"
+
+generator = torch.manual_seed(42)
+image = pipe(
+    prompt=prompt, num_inference_steps=4, generator=generator, guidance_scale=1.0
+).images[0]
+
+
+# save image
+from PIL import Image
+
+Image.fromarray(image).save(os.path.join(save_dir, "self-portrait-oil-painting.png"))
