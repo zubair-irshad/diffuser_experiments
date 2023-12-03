9bcf024e88100027e126f8a8972a814960e26070
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-12-03 18:01:42 -0500

    first commit

--- test_ipix2pix.py
+++ test_ipix2pix.py
@@ -221,7 +221,7 @@ elif edit_type == "lcm-lora-sdxl":
         "diffusers/sdxl-instructpix2pix-768", torch_dtype=torch.float16
     ).to("cuda")
 
-    pipe.load_lora_weights("latent-consistency/lcm-sdxl")
+    pipe.load_lora_weights("latent-consistency/lcm-lora-sdxl")
     # set scheduler
     pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
 
